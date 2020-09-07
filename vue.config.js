6b9c04d0e6b78aa3a5372d59cdf561d63e1b94af
KFC <hankfchan@gmail.com>
2020-09-07 11:03:20 +0800

    migrating to MC

--- vue.config.js
+++ vue.config.js
@@ -1,7 +1,7 @@
 module.exports = {
   publicPath:
     process.env.NODE_ENV === "production"
-      ? "https://api.greenpeace.org.hk/2020/petition/zh-hk.2020.general.plasticfreecity.general.signup.na.mc/"
+      ? "https://api.greenpeace.org.hk/2020/petition/zh-hk.2020.plastics.plasticfreecity.general.signup.na.mc/"
       : "/",
   css: {
     sourceMap: process.env.NODE_ENV !== "production" ? true : false,
